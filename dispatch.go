d600b26c8d7cb801ae370d546bcb56d55ede5002
Shyamin Ayesh <me@shyamin.com>
2021-03-27 00:55:48 +0530

    implement a way to dispatch requests

--- dispatch.go
+++ dispatch.go
@@ -1,9 +1,27 @@
 package zend
 
 import (
+	"fmt"
 	"net/http"
+	"io/ioutil"
 )
 
-func (c *Client) Dispatch(request http.Request) (error) {
-	return nil
+func (c *Client) Dispatch(request *http.Request) ([]byte, error) {
+
+	/**
+	* We have to set headers before we dispatch the http request
+	* to the origin to append authentication token, etc.
+	*/
+	request.Header.Set("Content-Type", "application/json")
+	request.Header.Set("User-Agent", "zend/go-sdk")
+	request.Header.Set("Authorization", fmt.Sprintf("Bearer %s", c.Token))
+
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil { return nil, err }
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil { return nil, err }
+
+	return body, nil
 }
