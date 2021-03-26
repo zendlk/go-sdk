2135889248223307f5faf336ed5ece594bfc6a89
Shyamin Ayesh <me@shyamin.com>
2021-03-26 20:20:59 +0530

    Return client

--- zend.go
+++ zend.go
@@ -4,6 +4,13 @@ import (
 	"fmt"
 )
 
+func NewClient(Token string, Sender string) *Client {
+	return &Client {
+		Token: Token,
+		Sender: Sender,
+	}
+}
+
 func (c *Client) Message(Recipient string, Message string) (error) {
 
 	fmt.Println("called to Message")
