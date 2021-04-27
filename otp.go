989f1801d0d121b27f39ce43ba4225003c518fe7
Shyamin Ayesh <me@shyamin.com>
2021-04-27 23:26:03 +0530

    OTP verification

--- otp.go
+++ otp.go
@@ -12,7 +12,7 @@ type OtpService struct {
 	client *Client
 }
 
-func (svc *OtpService) Request(recipient string) (*Otp, error) {
+func (svc *OtpService) Send(recipient string) (*Otp, error) {
 
 	/**
 	* We have to marshal struct with information containing for
@@ -65,4 +65,50 @@ func (svc *OtpService) Request(recipient string) (*Otp, error) {
 	 */
 	return nil, errors.New("unknown error")
 
+}
+
+func (svc *OtpService) Verify(otp_id string, code string) (bool, error) {
+
+	/**
+	* We have to marshal struct with information containing for
+	* message endpoint to provide it as the request body to the
+	* upstream.
+	 */
+	payload, err := json.Marshal(map[string]string{
+		"id":   otp_id,
+		"code": code,
+	})
+	if err != nil {
+		return false, err
+	}
+
+	request, err := http.NewRequest("POST", fmt.Sprintf("%s/v%s/otp/verify", svc.client.URI, svc.client.Version), bytes.NewBuffer(payload))
+	if err != nil {
+		return false, err
+	}
+
+	/**
+	* We can now safely dispatch the request to the upstream server
+	* for furthur processing and execution of the task.
+	 */
+	response, err := svc.client.Dispatch(request)
+	if err != nil {
+		return false, err
+	}
+
+	/**
+	* Handle the return response according to the API and return the
+	* response data back to the application.
+	 */
+	if response["status"] == "success" {
+		return true, nil
+	} else if response["status"] == "failed" {
+		return false, errors.New(response["error"].(map[string]interface{})["message"].(string))
+	}
+
+	/**
+	* We got unexpected state and we have to return general error
+	* back to the application.
+	 */
+	return false, errors.New("unknown error")
 }
